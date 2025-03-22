fdf2e63530b09b95bb12924b172313d383cf66f6
DominikGarsia <0dominikgarsia0@gmail.com>
2025-03-22 11:00:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function openResume(url) {
     window.location.href = url;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector(".cards-container");
+
+    if (container) {
+        container.addEventListener("wheel", function (event) {
+            event.preventDefault(); // Отменяем стандартную вертикальную прокрутку
+            container.scrollLeft += event.deltaY; // Двигаем карточки влево/вправо
+        });
+    }
+});
